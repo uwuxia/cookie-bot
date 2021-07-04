77502ff0082cd10b5aa4e26185e54c16f5457d46
dogeduaa <quzabubaker@gmail.com>
2021-07-05 02:43:58 +0300

    added image search and google search

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import Discord from 'discord.js';
 import dotenv from 'dotenv';
 import fs from 'fs';
 
+import fetch from 'node-fetch';
+
 dotenv.config();
 const bot = new Discord.Client();
 
@@ -75,6 +77,57 @@ bot.on('message', (message) => {
         } else {
             message.channel.send("Only cookie can add facts <:tea:856945220804804611>");
         }
+    }
+
+    if (message.content.includes("cookie search")) {
+        const query = message.content.split('').splice(13).join('');
+        async function googleSearch() {
+            const data = await fetch("https://google-search3.p.rapidapi.com/api/v1/search/q=" + query, {
+                "method": "GET",
+                "headers": {
+                    "x-rapidapi-key": "857626ae3emsh3d0a744b1797271p1fbd39jsn0c6ab4e4751d",
+                    "x-rapidapi-host": "google-search3.p.rapidapi.com"
+                }
+            });
+        
+            const response = await data.json();
+        
+            let result = ``;
+        
+            const results = response.results.splice(0, 3);
+
+            results.forEach(res => {
+                result += res.title + "\n" + res.description + "\n" + "<" + res.link + ">" + "\n\n";
+            });
+        
+            message.channel.send(result);
+        }
+
+        googleSearch()
+    }
+
+    if (message.content.includes("cookie img")) {
+        const query = message.content.split('').splice(10).join('');
+        async function imageSearch() {
+            const data = await fetch("https://bing-image-search1.p.rapidapi.com/images/search?q=" + query, {
+                "method": "GET",
+                "headers": {
+                    "x-rapidapi-key": "857626ae3emsh3d0a744b1797271p1fbd39jsn0c6ab4e4751d",
+                    "x-rapidapi-host": "bing-image-search1.p.rapidapi.com"
+                }
+            })
+
+            const response = await data.json();
+
+            const results =  [];
+
+            response.value.forEach(img => {
+                results.push(img.contentUrl);
+            });
+
+            message.channel.send(results[Math.floor(Math.random() * results.length)]);
+        }
+        imageSearch();
     }
 });
 
